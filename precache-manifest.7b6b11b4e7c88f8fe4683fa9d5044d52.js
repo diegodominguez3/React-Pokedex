0d29c8b79234f242024c209d97afa2f3cca0fc22
Diego Dominguez <diego.dominguez@gammapartners.com>
2020-09-10 13:10:45 -0600

    Updates

---
+++ precache-manifest.7b6b11b4e7c88f8fe4683fa9d5044d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be56f16bee769208fea0fbbdd5cfd575",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "1e7150899248cc701ed8",
+    "url": "/react-pokedex/static/js/2.24981384.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.24981384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514f33fe685142c82c70",
+    "url": "/react-pokedex/static/js/main.b8b008ad.chunk.js"
+  },
+  {
+    "revision": "8d0dc51f96d551a6d0f3",
+    "url": "/react-pokedex/static/js/runtime-main.f27a760f.js"
+  },
+  {
+    "revision": "6f1fd44625c52ccd693ca4e39b23cc9f",
+    "url": "/react-pokedex/static/media/pokedex-logo.6f1fd446.png"
+  }
+]);
